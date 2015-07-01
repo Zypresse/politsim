d75316ac0b65bffa5ba082a93705715ca102ae2b
Ilya Gorokhov <tairesh.rus@gmail.com>
2015-07-01 14:27:12 +0500

    fix

---
+++ config/params.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'adminEmail' => 'tairesh.rus@gmail.com',
+    'AUTH_KEY_SECRET' => '_FUCKRIVALREGIONS',
+];
---
+++ config/web.php
@@ -0,0 +1,82 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'cbykD2ZgRg9u4-8MFHu-Xm0913geyLHy',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'json/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\GoogleOAuth',
+                    'clientId' => '95701989043-8dh4etcl3dheudfs156mg131ftd0pbpv.apps.googleusercontent.com',
+                    'clientSecret' => 'oxmR-jiruIs0Qi0aLbFu3fuq',
+                ],
+                'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => '403421213132291',
+                    'clientSecret' => '23215c743e6e5e6c408d4249402f5dc5',
+                ],
+                'vkontakte' => [
+                    'class' => 'yii\authclient\clients\VKontakte',
+                    'clientId' => '4540646',
+                    'clientSecret' => 'xtJOMSSdAtk7qcIO9z2H',
+                    'title' => 'VK'
+                ],
+            ],
+        ]
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+//        'allowedIPs' => ['127.0.0.1', '::1', '*'],
+    ];
+}
+
+return $config;
