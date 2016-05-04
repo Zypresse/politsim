2960c6af10ebc4da271b3a9a12db4544f0a2e8f0
Ilya Gorokhov <tairesh.rus@gmail.com>
2016-05-04 23:22:59 +0500

    tmp

--- commands/TestController.php
+++ commands/TestController.php
@@ -2,12 +2,20 @@
 
 namespace app\commands;
 
-use yii\console\Controller;
+use yii\console\Controller,
+    app\models\licenses\LicenseRule;
 
 class TestController extends Controller
 {
     public function actionIndex()
     {
         echo "test";
+        $licenses = LicenseRule::find()->all();
+        foreach ($licenses as $l) {
+            /* @var $l LicenseRule */
+            if (!$l->proto) {
+                $l->delete();
+            }
+        }
     }
 }
