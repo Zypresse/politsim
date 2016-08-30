e886b0b530a1959cdcedf59c2eb9591c6e1307a4
Ilya Gorokhov <tairesh.rus@gmail.com>
2016-08-30 19:37:33 +0500

    модель гражданства

---
+++ models/Citizenship.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models;
+
+use Yii,
+    app\components\MyModel;
+
+/**
+ * Гражданство
+ *
+ * @property integer $id
+ * @property integer $userId
+ * @property integer $stateId
+ * @property integer $dateCreated
+ * @property integer $dateApproved
+ * 
+ * @property User $user
+ * @property State $state
+ */
+class Citizenship extends MyModel {
+    
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'accounts';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['userId', 'stateId'], 'required'],
+            [['userId', 'stateId', 'dateCreated', 'dateApproved'], 'integer', 'min' => 0],
+        ];
+    }
+    
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), array('id' => 'userId'));
+    }
+    
+    public function getState()
+    {
+        return $this->hasOne(State::className(), array('id' => 'stateId'));
+    }
+    
+    public function beforeSave($insert)
+    {
+        if ($insert) {
+            $this->dateCreated = time();
+        }
+        return parent::beforeSave($insert);
+    }
+    
+}
