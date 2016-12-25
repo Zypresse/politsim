679c38550970dd5264b75ae4730476d4e3db0b5c
Ilya Agafonov <tairesh.rus@gmail.com>
2016-12-25 16:49:01 +0500

    просто говнокоммит чтобы не проебать воскресенье

--- views/widgets/election-info/ended.php
+++ views/widgets/election-info/ended.php
@@ -3,6 +3,20 @@
 /* @var $election \app\models\politics\elections\Election */
 /* @var $viewer \app\models\User */
 
+$data = json_decode($election->results);
+$data->results = (array) $data->results;
+
+$maxVotes = 0;
+$sumVotes = 0;
+foreach ($data->results as $variant => $votes) {
+    if ($votes > $maxVotes) {
+        $winner = $variant;
+        $maxVotes = $votes;
+    }
+    $sumVotes += $votes;
+}
+$winner = app\models\politics\elections\ElectionRequest::find()->where(['electionId' => $election->id, 'variant' => $winner])->one();
+
 ?>
 <div class="row">
     <div class="col-md-6 col-sm-12">
@@ -10,6 +24,6 @@
         <p><strong><?=Yii::t('app', 'Voting finish:')?></strong> <span class="formatDate" data-unixtime="<?=$election->dateVotingEnd?>"><?=date('d-m-y', $election->dateVotingEnd)?></span></p>
     </div>
     <div class="col-md-6 col-sm-12">
-        <pre><?=print_r(json_decode($election->results))?></pre>
+        <pre><?=print_r($winner)?></pre>
     </div>
 </div>
