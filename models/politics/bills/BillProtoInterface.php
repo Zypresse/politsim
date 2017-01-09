5e1fb60ec1443f6284fb89a109c2786d89b40134
Ilya Agafonov <tairesh.rus@gmail.com>
2017-01-09 18:50:03 +0500

    модели переименования государства

--- models/politics/bills/BillProtoInterface.php
+++ models/politics/bills/BillProtoInterface.php
@@ -2,10 +2,25 @@
 
 namespace app\models\politics\bills;
 
+use app\models\politics\bills\Bill;
+
 /**
  *
  */
 interface BillProtoInterface
 {
     
+    /**
+     * 
+     * @param Bill $bill
+     */
+    public function validate($bill) : bool;
+    
+    
+    /**
+     * 
+     * @param Bill $bill
+     */
+    public function accept($bill) : bool;
+        
 }
--- models/politics/bills/prototypes/RenameState.php
+++ models/politics/bills/prototypes/RenameState.php
@@ -2,12 +2,37 @@
 
 namespace app\models\politics\bills\prototypes;
 
-use app\models\politics\bills\BillProtoInterface;
+use app\models\politics\bills\BillProtoInterface,
+    app\models\politics\bills\Bill,
+    app\models\politics\State;
 
 /**
  * Переименование государства
  */
 class RenameState implements BillProtoInterface
 {
-    //put your code here
+    
+    /**
+     * 
+     * @param Bill $bill
+     */
+    public function accept($bill) : bool
+    {
+        $data = json_decode($bill->data);
+        $bill->state->name = $data->name;
+        $bill->state->nameShort = $data->nameShort;
+        $bill->state->save();
+        return true;
+    }
+
+    /**
+     * 
+     * @param Bill $bill
+     */
+    public function validate($bill) : bool
+    {
+        $data = json_decode($bill->data);
+        return ($data->name && $data->nameShort);
+    }
+
 }
