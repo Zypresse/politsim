cc701f6af10db9cdc368d3f7e6a78943ecfbdfd5
Ilya Agafonov <tairesh.rus@gmail.com>
2017-12-23 17:47:53 +0500

    assets fix

---
+++ assets/LandingAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\assets;
+
+use Yii,
+    yii\web\AssetBundle;
+
+class LandingAsset extends AssetBundle
+{
+
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+	'/css/landing.css',
+    ];
+    public $js = [
+	'/js/jquery.countdown.min.js',
+	'/js/landing.js',
+    ];
+    public $depends = [
+//	'yii\web\YiiAsset',
+//	'yii\bootstrap\BootstrapAsset',
+    ];
+
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,12 +1,13 @@
 <?php
 
 use yii\helpers\Html;
-use app\assets\AppAsset;
+use app\assets\LandingAsset;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-AppAsset::register($this);
+LandingAsset::register($this);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -16,12 +17,12 @@ AppAsset::register($this);
         <meta name="viewport" content="width=device-width, initial-scale=1">
 	<?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
-	<?php $this->head() ?>
+<?php $this->head() ?>
     </head>
     <body>
 	<?php $this->beginBody() ?>
 	<?= $content ?>
-	<?php $this->endBody() ?>
+<?php $this->endBody() ?>
     </body>
 </html>
 <?php $this->endPage() ?>
