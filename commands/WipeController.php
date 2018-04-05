bedcc425cb0f6b6e2186cb7d06ea1eff795cc746
Ilya Agafonov <ilia.a@aio.media>
2018-04-05 15:00:33 +0500

    map cleared and fixed

--- commands/WipeController.php
+++ commands/WipeController.php
@@ -140,31 +140,31 @@ class WipeController extends Controller
     public function actionCombinePolygons()
     {
         $db = Yii::$app->db;
-//        $db->createCommand()->setSql("TRUNCATE TABLE {$db->quoteTableName(Polygon::tableName())} CASCADE")->execute();
-//        $cities = City::findAll();
-//        /* @var $city City */
-//        foreach ($cities as $city) {
-//            echo $city->name;
-//            if ($city->getPolygon()->exists()) {
-//                echo " skipped".PHP_EOL;
-//                continue;
-//            }
-//            if (!$city->getTiles()->exists()) {
-//                echo " have no tiles".PHP_EOL;
-//                continue;
-//            }
-//            $polygon = new Polygon([
-//                'ownerType' => Polygon::TYPE_CITY,
-//                'ownerId' => $city->id,
-//                'data' => TileCombiner::combine($city->getTiles()),
-//            ]);
-//            if (!$polygon->save()) {
-//                var_dump($polygon->getErrors(), $polygon->data); die();
-//            }
-//            echo " saved".PHP_EOL;
-//        }
+        $db->createCommand()->setSql("TRUNCATE TABLE {$db->quoteTableName(Polygon::tableName())} CASCADE")->execute();
+        $cities = City::findAll();
+        /* @var $city City */
+        foreach ($cities as $city) {
+            echo $city->name;
+            if ($city->getPolygon()->exists()) {
+                echo " skipped".PHP_EOL;
+                continue;
+            }
+            if (!$city->getTiles()->exists()) {
+                echo " have no tiles".PHP_EOL;
+                continue;
+            }
+            $polygon = new Polygon([
+                'ownerType' => Polygon::TYPE_CITY,
+                'ownerId' => $city->id,
+                'data' => TileCombiner::combine($city->getTiles()),
+            ]);
+            if (!$polygon->save()) {
+                var_dump($polygon->getErrors(), $polygon->data); die();
+            }
+            echo " saved".PHP_EOL;
+        }
         
-        $regions = Region::find()->where(['>', 'id', 1261])->all();
+        $regions = Region::findAll();
         /* @var $region Region */
         foreach ($regions as $region) {
             echo $region->name;
