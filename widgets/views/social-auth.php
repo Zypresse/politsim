4634462f2f2018be7cbf6104338fa0a5ab410249
Ilya Agafonov <ilia.a@aio.media>
2018-04-12 19:47:28 +0500

    fix social-auth widget design

--- widgets/views/social-auth.php
+++ widgets/views/social-auth.php
@@ -7,7 +7,7 @@ use app\helpers\Html;
 
 ?>
 <div class="social-auth-links  <?= $inline ? '' : 'text-center' ?>">
-    <?= Html::a('<i class="fa fa-vk"></i> Войти через VK', ['/auth/auth', 'authclient' => 'vkontakte'], ['class' => 'btn btn-social btn-vk btn-flat' . ($inline ? 'btn-block' : '')]) ?>
-    <?= Html::a('<i class="fa fa-facebook"></i> Войти через Facebook', "https://deletefacebook.com/" /* ['/auth/auth', 'authclient' => 'facebook'] */, ['class' => 'btn btn-social btn-facebook btn-flat' . ($inline ? 'btn-block' : '')]) ?>
-    <?= Html::a('<i class="fa fa-google-plus"></i> Войти через Google+', ['/auth/auth', 'authclient' => 'google'], ['class' => 'btn btn-social btn-google btn-flat' . ($inline ? 'btn-block' : '')]) ?>
+    <?= Html::a('<i class="fa fa-vk"></i> Войти через VK', ['/auth/auth', 'authclient' => 'vkontakte'], ['class' => 'btn btn-social btn-vk btn-flat ' . ($inline ? '' : 'btn-block')]) ?>
+    <?= Html::a('<i class="fa fa-facebook"></i> Войти через Facebook', "https://deletefacebook.com/" /* ['/auth/auth', 'authclient' => 'facebook'] */, ['class' => 'btn btn-social btn-facebook btn-flat ' . ($inline ? '' : 'btn-block')]) ?>
+    <?= Html::a('<i class="fa fa-google-plus"></i> Войти через Google+', ['/auth/auth', 'authclient' => 'google'], ['class' => 'btn btn-social btn-google btn-flat ' . ($inline ? '' : 'btn-block')]) ?>
 </div>
